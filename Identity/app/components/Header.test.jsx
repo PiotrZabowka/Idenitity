421495875cd790bdf8dca911a7bbd2710cbe600f
Piotr Zabowka <pz@miinto.com>
2017-03-01 23:08:05 +0100

    enzyme tests

--- Identity/app/components/Header.test.jsx
+++ Identity/app/components/Header.test.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import {shallow} from 'enzyme';
 import renderer from 'react-test-renderer';
 import Header from './Header';
 
-test('Link changes the class when hovered', () => {
-  const component = renderer.create(
-    <Header>Facebook</Header>,
-  );
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+describe('<Header />', () => {
+  it('Link changes the class when hovered', () => {
+    const component = renderer.create(
+      <Header>Facebook</Header>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  it('reders h1', () => {
+    const header = shallow(<Header>Facebook</Header>);
+    expect(header.find('h1').length).toBe(1);
+  });
 });
